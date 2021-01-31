953f07861cfb4faea02fe9839b0e6808fa9bb4c5
Kane G <kane@plustime.com.au>
2021-01-31 17:30:31 +1000

    0.0.5

--- src/install.js
+++ src/install.js
@@ -1,13 +1,13 @@
 import FormLoader from "./components/forms/FormLoader.vue";
 
 const S2NEasyForms = {
-  install(Vue) {
-    Vue.component("form-loader", FormLoader);
+  install( Vue ) {
+    Vue.component( "form-loader" , FormLoader );
   }
 };
 
-if (typeof window !== "undefined" && window.Vue) {
-  window.Vue.use(S2NEasyForms);
+if ( typeof window !== "undefined" && window.Vue ) {
+  window.Vue.use( S2NEasyForms );
 }
 
 export default S2NEasyForms;
